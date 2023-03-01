40a446e51210c569bae974eb462d51317c52ffae
Lauri <lauriaslak@gmail.com>
2023-03-01 21:29:03 +0200

    Finished basic info page

--- asklepio-web/src/components/Footer.tsx
+++ asklepio-web/src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import "../style/components/Footer.css";
 import asklepio from "../assets/images/logos/asklepio_logo.png";
+import { ReactComponent as Instagram } from "../assets/icons/instagram.svg";
+import { ReactComponent as Mail } from "../assets/icons/mail.svg";
 
 export const Footer = () => {
   return (
@@ -9,21 +11,7 @@ export const Footer = () => {
       <ul>
         <li>
           <a href="mailto:asklepio@utu.fi" target="_blank" rel="noreferrer">
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"
-                stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <Mail />
             asklepio[ät]utu.fi
           </a>
         </li>
@@ -33,21 +21,7 @@ export const Footer = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16.65 7.2H16.66M8 20H16C18.2091 20 20 18.2091 20 16V8C20 5.79086 18.2091 4 16 4H8C5.79086 4 4 5.79086 4 8V16C4 18.2091 5.79086 20 8 20ZM15.75 12C15.75 14.0711 14.0711 15.75 12 15.75C9.92893 15.75 8.25 14.0711 8.25 12C8.25 9.92893 9.92893 8.25 12 8.25C14.0711 8.25 15.75 9.92893 15.75 12Z"
-                stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <Instagram />
             Asklepio.ry
           </a>
         </li>
--- asklepio-web/src/pages/Home.tsx
+++ asklepio-web/src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import asklepio from "../assets/images/logos/asklepio_logo.png";
+
 export default function Home() {
   return (
     <>
-      <h1>Asklepio ry</h1>
+      <img src={asklepio} alt="Logo of Asklepio ry" />
       <h2>
         Lääketieteellisen tekniikan ja terveysteknologian teekkarikilta Asklepio
         ry
@@ -11,9 +13,31 @@ export default function Home() {
         tekniikan ja terveysteknologian DI-opiskelijoiden tieteellistä ja
         ammatillista kehitystä, vaalia opinto- ja ammattikysymyksiin liittyviä
         yleisiä ja yhteisiä etuja, ja viihtyvyyttä sekä edistää jäsenten
-        mielekkäitä vapaa-ajan viettomahdollisuuksia. Kilta on perustettu
-        11.11.2022.
+        mielekkäitä vapaa-ajan viettomahdollisuuksia.
+        <br />
+        <br />
+        Kilta on perustettu 11.11.2022.
       </p>
+      <h3>Hallitus 2023</h3>
+      <h4>Puheenjohtaja</h4>
+      <p>Jani Norrby</p>
+      <h4>Varapuheenjohtaja</h4>
+      <p>Roosa Hietalahti</p>
+      <h4>Sihteeri</h4>
+      <p>Inka Mustajoki</p>
+      <h4>Taloudenhoitaja</h4>
+      <p>Maria Säilä</p>
+      <h4>Tapahtumavastaava</h4>
+      <p>Noora Räkköläinen</p>
+      <h4>Kiltahuonevastaava</h4>
+      <p>Anni Halkilahti</p>
+      <h4>Fuksi- ja tuutorivastaava</h4>
+      <p>Tessa Salmia</p>
+      <h4>Koulutuspoliittinen vastaava</h4>
+      <p>Saara Mäkelä</p>
+      <h4>Ulkoasianvastaava</h4>
+      <p>Lauri Suominen</p>
+      <span>Sivuja kehitetään...</span>
     </>
   );
 }
