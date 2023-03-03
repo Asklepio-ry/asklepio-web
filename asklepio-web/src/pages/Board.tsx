87d919c180cbd4682834440909ff4d004e056377
Lauri <lauriaslak@gmail.com>
2023-03-03 23:33:56 +0200

    Did stuff

--- asklepio-web/src/pages/Board.tsx
+++ asklepio-web/src/pages/Board.tsx
@@ -1,3 +1,32 @@
 export default function Board() {
-  return <></>;
+  return (
+    <>
+      <p>
+        Koko hallituksen tavoittaa sähköpostitse{" "}
+        <a href="mailto:asklepio@utu.fi">asklepio[ät]utu.fi</a>
+        <br />
+        <br />
+        Henkilökohtaiset osoitteet tunnus[ät]utu.fi
+      </p>
+      <h3>Hallitus 2023</h3>
+      <h4>Puheenjohtaja</h4>
+      <p>Jani Norrby</p>
+      <h4>Varapuheenjohtaja</h4>
+      <p>Roosa Hietalahti</p>
+      <h4>Sihteeri</h4>
+      <p>Inka Mustajoki</p>
+      <h4>Taloudenhoitaja</h4>
+      <p>Maria Säilä</p>
+      <h4>Tapahtumavastaava</h4>
+      <p>Noora Räkköläinen</p>
+      <h4>Kiltahuonevastaava</h4>
+      <p>Anni Halkilahti</p>
+      <h4>Fuksi- ja tuutorivastaava</h4>
+      <p>Tessa Salmia</p>
+      <h4>Koulutuspoliittinen vastaava</h4>
+      <p>Saara Mäkelä</p>
+      <h4>Ulkoasianvastaava</h4>
+      <p>Lauri Suominen</p>
+    </>
+  );
 }
