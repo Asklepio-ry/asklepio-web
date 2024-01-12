57c72de2329c480e1809b71cc38ce3f803ee9989
Sini <sini.lahde@gmail.com>
2024-01-12 16:48:38 +0200

    Uusi setti

--- asklepio-web/src/pages/Board.tsx
+++ asklepio-web/src/pages/Board.tsx
@@ -21,50 +21,60 @@ export default function Board() {
             email="maria.saila@asklepio.fi"
             telegram="mariasaila"
           />
+
           <BoardMemberCard
             name="Charlotta Huima"
             title="Varapuheenjohtaja"
             email="charlotta.huima@asklepio.fi"
             telegram=""
           />
+
           <BoardMemberCard
             name="Eeli Hinkka"
             title="Sihteeri"
             email="eeli.hinkka@asklepio.fi"
             telegram="eelihinkka"
           />
+
           <BoardMemberCard
             name="Matilda Laurila"
             title="Taloudenhoitaja"
             email="matilda.laurila@asklepio.fi"
             telegram="matskuuuw"
           />
+
           <BoardMemberCard
             name="Silja Kuusirati"
             title="Tapahtumavastaava"
             email="silja.kuusirati@asklepio.fi"
             telegram="siljakuusirati"
           />
+          
         </div>
+
         <div className="card-column">
+
           <BoardMemberCard
             name="Sini Lähde"
             title="Some- ja viestintävastaava"
             email="sini.lahde@asklepio.fi"
             telegram="sinilahde"
           />
+
           <BoardMemberCard
             name="Tessa Salmia"
             title="Fuksi- ja tuutorivastaava"
             email="tessa.salmia@asklepio.fi"
             telegram="padawanito"
           />
+
           <BoardMemberCard
             name="Saara Mäkelä"
             title="Koulutuspoliittinen vastaava"
             email="saara.makela@asklepio.fi"
             telegram="sarammbo"
           />
+
           <BoardMemberCard
             name="Tuukka Lehtomäki"
             title="Ulkoasianvastaava"
